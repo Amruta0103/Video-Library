9cb8fa3ecb7a423d1c18b997efc308e8c67c90b2
Amruta_D <amydharap@gmail.com>
2021-06-02 20:45:35 +0530

    updating UI (2)

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,31 @@ import Liked from "./likedVideos";
 import Saved from "./SavedVideos";
 import WatchLater from "./WatchLaterVideos";
 import AllVideos from './AllVideos';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return(
     <div className="App">
       <nav className="nav">
         <div className="logo-box">
-          <img className="logo" src="./favicon.ico" alt="logo"></img>
+          <img className="logo" src="./imageStocks/favicon.ico" alt="logo"></img>
           <h1>my vid lib</h1>
         </div>
         <div className="nav-menu">
-          <div className="nav-menu-option" >BROWSE</div>
-          <div className="nav-menu-option" >WATCH LATER</div>
-          <div className="nav-menu-option" >SAVED</div>
-          <div className="nav-menu-option" >LIKED</div>
+          <Link to="/" ><button className="nav-menu-option" >BROWSE</button></Link>
+          <Link to="/watchLater" ><button className="nav-menu-option" >WATCH LATER</button></Link>
+          <Link to="/savedVideos" ><button className="nav-menu-option" >SAVED</button></Link>
+          <Link to="/likedVideos" ><button className="nav-menu-option" >LIKED</button></Link>
         </div>
       </nav>
-      <Liked />
-      <Saved />
-      <WatchLater />
-      <AllVideos />
+
+      <Routes>
+        <Route path="/" element={<AllVideos />} />
+        <Route path="/likedVideos" element={<Liked />} />
+        <Route path="/savedVideos" element={<Saved />} />
+        <Route path="/watchLater" element={<WatchLater />} />
+      </Routes>
+
     </div>
   );
 }
--- src/video.js
+++ src/video.js
@@ -4,16 +4,16 @@ export default function Video(){
   return(
     <div className="Video">
       <div className="thumbnail-box" >
-      <img src="./Selfie.png" className="thumbnail" alt="thumbnail"></img>
+      <img src="./imageStocks/Selfie.png" className="thumbnail" alt="thumbnail"></img>
       <div className="duration-stamp">16:25</div>
       </div>
       <div className="description">
       <h2>vid Name</h2>
       <p>description</p>
       </div>
-      <div>
+      {/* <div>
         <img style={{ width: "1.5rem"}} src="./menu-dots-horizontal.svg" alt="menu"></img>
-      </div>
+      </div> */}
     </div>
   );
 }
